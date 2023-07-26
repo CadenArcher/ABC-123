7e4cba031ca5c23173fa49a93bf7ab7a3ac477c7
CadenArcher <cadenarcher45@gmail.com>
2023-07-26 19:25:43 -0400

    a function that returns the int of a wanted number by using static_cast to make it into a double before deviding it by half and return the new int rounder

--- ABC 123/ABC 123.cpp
+++ ABC 123/ABC 123.cpp
@@ -1,9 +1,22 @@
-
-
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+template <typename H>
+
+H Half(H InputNumber) {
+    cout << InputNumber / 2;
+}
+
+int Half(int InputNumber) {
+    return round(static_cast<double>(InputNumber) / 2);
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
