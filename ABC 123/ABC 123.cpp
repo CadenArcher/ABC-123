81084d2c026e9ef8127e03a516b7a529b0f8adf6
CadenArcher <cadenarcher45@gmail.com>
2023-07-30 12:22:54 -0400

    Added comments to explain what is happening in the program

--- ABC 123/ABC 123.cpp
+++ ABC 123/ABC 123.cpp
@@ -1,3 +1,12 @@
+/*
+Caden Archer
+7/30/2023
+Name; ABC 123
+Discription: Using a template I devide numbers by half
+
+*/
+
+
 #include <iostream>
 #include <cmath>
 
@@ -6,12 +15,12 @@ using namespace std;
 
 template <typename H>
 
-H Half(H InputNumber) {
-    return InputNumber / 2;
+H Half(H InputNumber) {//divide the number in half
+     InputNumber / 2;
 }
 
 int Half(int InputNumber) {
-    return round(static_cast<double>(InputNumber) / 2);
+    return round(static_cast<double>(InputNumber));//returns a number rounded in half if its a int only
 
 }
 
